4cd62e7b8888278648285a8533c3c2786bc740f2
STR4WNIE <strawnie3766@gmail.com>
2025-06-22 20:45:11 +1000

    gay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 class="main heads">
+  <div>
+    <header>
+        <img src="images/ABGL logo.png" alt="ABGL logo"/>
+        <nav>
+            <a href="PastTour.html">Past Tournaments</a>
+            <a href="Leaderboard.html">Leaderboard</a>
+            <a href="NextTour.html">Next Tournament</a>
+        </nav>
+    </header>
+
+    <h1>
         The ABGL Tour
     </h1>
 
-    <main class="main">
-            <div class="about-tour">
+    <main>
                 <p> The ABGL tour was created to celebrate the gmae of Golf and the connection of friends and family, this tournament will be played monthly. A venue will be appointed by
                     the founders of the competition - Sir Harry and Sir Jacob. This is an invitational event, we will also play with a handicap. We encourage participation.
                 </p>
-            </div>
     </main>
+    </div>
   );
 }
