bfa0d646cd826909d3f68de23246716b8f529d2e
STR4WNIE <strawnie3766@gmail.com>
2025-06-25 23:48:42 +1000

    added images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,15 +17,37 @@ export default function Home() {
         <div className="past-events">
           <div className="event-card">
             <p>Past Event 1:</p>
-            <img src="/ABGL logo.png" alt="Past Event 1" className="event-image" />
+            <img src="/test_image_1.jpg" alt="Past Event 1" className="event-image" />
+              <div>
+                <p> Quick description of the round goes here, including the date, course name, total number of players and the overall winner.</p>
+                <p>Div 1. Winner: blah blah</p>
+                <p>Div 2. Winner: blah blah</p>
+                <p>Div 3. Winner: blah blah</p>
+                <p>Div 4. Winner: blah blah</p>
+              </div>
           </div>
           <div className="event-card">
             <p>Past Event 2:</p>
-            <img src="/ABGL logo.png" alt="Past Event 2" className="event-image" />
+            <img src="/test_image_2.jpg" alt="Past Event 2" className="event-image" />
+              <div>
+                <p> Quick description of the round goes here, including the date, course name, total number of players and the overall winner.</p>
+                <p>Div 1. Winner: blah blah</p>
+                <p>Div 2. Winner: blah blah</p>
+                <p>Div 3. Winner: blah blah</p>
+                <p>Div 4. Winner: blah blah</p>
+              </div>
           </div>
+            
           <div className="event-card">
             <p>Past Event 3:</p>
-            <img src="/ABGL logo.png" alt="Past Event 3" className="event-image" />
+            <img src="/test_image_3.jpg" alt="Past Event 3" className="event-image" />
+              <div>
+                <p> Quick description of the round goes here, including the date, course name, total number of players and the overall winner.</p>
+                <p>Div 1. Winner: blah blah</p>
+                <p>Div 2. Winner: blah blah</p>
+                <p>Div 3. Winner: blah blah</p>
+                <p>Div 4. Winner: blah blah</p>
+              </div>
           </div>
         </div>
       </div>
