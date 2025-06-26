5330a9c85e7958fa09e9b554a0cfb15b0ca498ec
STR4WNIE <strawnie3766@gmail.com>
2025-06-27 01:15:49 +1000

    added stuff to rules page

--- src/app/rules/page.tsx
+++ src/app/rules/page.tsx
@@ -1,11 +1,62 @@
-import HeaderBar from "@/components/HeaderBar"
+import HeaderBar from "@/components/headerBar"
 
 export default function Rules() {
     return (
         <div>
             <HeaderBar></HeaderBar>
+        <div className="flex flex-col items-center p-6 gap-6">
+            {/* Image Card */}
+            <div className="w-full max-w-4xl rounded-2xl shadow-lg overflow-hidden">
+                <img
+                src="/rules_banner.jpg"
+                alt="Rules Banner"
+                className="w-full h-auto object-cover"
+                />
+            </div>
 
-            <p>This is the rules page</p>
+            {/* Rules Box */}
+            <div className="flex flex-col gap-4 w-full max-w-3xl p-6 border border-gray-300 rounded-2xl shadow-md bg-white text-color-black">
+                <ul>
+                    <li>1. No Mulligans
+You get one shot, one opportunity… are you gonna capture it or let it slip? Do not give people in your group mulligans, do not take mulligans. There are no breakfast balls. There is a national competition at hand, don’t be a cheater.</li>
+
+<li>2. No Gimmes
+Putt the ball out. Putt every ball out. Even Tiger missed a three-footer, once. Do not give people in your group gimmes, none of you are good enough for any putt to be guaranteed. Again, there is a national competition at hand, keep it honest and putt out every ball.</li>
+
+<li>3. OBs and Hazards Both Play as Lateral Hazards
+A player is assessed a one stroke penalty drop for hitting into a hazard, out of bounds or losing a ball. All stakes play as lateral hazards. You may not rehit your previous shot. For your drop, the player may drop the ball within two club lengths, lateral to the point of entry into the hazard. To be clear: count your regular strokes and then ADD ONE MORE STROKE to that total before recording it.</li>
+
+<li>4. Lost Balls
+You have three minutes to find your ball. If you can’t find your ball, you are assessed a one stroke penalty and must drop your ball within two club lengths of where the ball was suspected to have come to rest. It does not matter if your whole group says “man, really thought your ball would be right here.” If you can’t find your ball, it’s a lost ball, and it’s a stroke penalty. And no, someone else did not hit or take your ball, you’re playing a Srixon, bud, no one stole it. If you lose a ball, and play a dropped ball, then proceed to find your first lost ball, pick it up and put it in your bag. You have to play your dropped ball and take the stroke penalty even if you subsequently find your first ball.</li>
+
+<li>5. Winter Rules/Fluffing Your Lie
+We play a variation of winter rules year round, meaning a player can pick up their ball and replace it within 6 inches of its original lie (except it cannot be moved from its original location on the green). However, you cannot improve your lie by changing the playing conditions of the golf ball. If your ball lies in the rough, it must be hit out of the rough. If it lies in the fairway, it must be hit from the fairway.</li>
+
+<li>6. Guys Play White Tees, Ladies Play Red Tees
+For most courses, the tees we play from are the whites and the reds. Some courses use different colors, but play from the “middle tees” and “forward tees.” The course yardage equivalent for middle tees should be approximately 5900-6200. For forward tees it should be approximately 5000-5500 yards.</li>
+
+<li>7. Cheating
+Beer League Golf does not tolerate cheaters. We are not your high school girlfriend or boyfriend and you don’t get a second chance. If you catch someone cheating, report it. Cheaters will not receive the prize. Cheaters will not be eligible to play in Beer League Golf events. Cheaters will not prosper.</li>
+
+<li>8. Drink Responsibly
+Beer League Golf’s “suggested drinking rules” are merely suggestions. You are not required to drink alcohol, drink a Capri Sun if you prefer. If you don’t want to drink anything at all, that’s fine too, but please consider drinking a water on occasion. If you do choose to drink alcohol, drink responsibly. Do not drink and drive. Call an Uber/Lyft or get a ride from a friend.</li>
+
+<li>8. Enter Your Score Every Hole
+Your scores must be entered on the Beer League Golf Web App after every hole. We enter our scores every hole so that the live Leaderboard for your Club updates everyone’s scores as the game is played. That way you can see who is leading and how much work have to do to catch them or protect your lead. If you don’t enter your score every hole, it won’t update your score on the live Leaderboard, people will be mad at you, you will lose friends, just enter your score every hole.</li>
+
+<li>9. Play Ready Golf
+Don’t wait on the guy dicking around trying to zip the difference of 228 or 232. He ain’t hitting the green anyhow. Grip it and rip it like the lord of golf himself, John Daly.</li>
+
+<li>10. Don’t be a Jerk
+This isn’t Men’s League and this sure as hell isn’t the PGA, so don’t be a dick and argue about every little thing. Keep your play honest, and follow the rules, but don’t make everyone miserable, Karen. Finish your drink and pick up your trash.</li>
+
+<li>11. Respect The Course
+If you rock a 60 degree wedge and take a fat beaver pelt divot which results with your ball cratering the green like Sputnik take the time to fix the damage. Also, support your course by making sure to buy your beverages and snacks from the restaurant and remember that Beer Leaguer's have big tip energy, tip your bartenders!!</li>
+
+<li>For all other situations that might arise - refer to the hosting course rules or your local league leadership. If you still have questions after thoroughly consulting all of the aforementioned resources, consult your inner self and your higher power.</li>
+                </ul>
+            </div>
+            </div>
         </div>
     )
 }
