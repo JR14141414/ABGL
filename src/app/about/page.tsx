708e5cb2255c326f65ab3ca0f424532d11f0b2b9
STR4WNIE <strawnie3766@gmail.com>
2025-06-28 03:39:27 +1000

    started about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,89 @@
-import HeaderBar from "@/components/HeaderBar"
+import HeaderBar from "@/components/headerBar"
 
 export default function About() {
     return (
         <div>
             <HeaderBar></HeaderBar>
+<div style={{ padding: '50px', fontSize: '16px', lineHeight: '1.6' }}>
+      <p>
+        <img
+          src="/test_image_1.jpg"
+          alt="Left image"
+          className="float-left max-w-[15%] h-auto mr-4 mb-2"
+        />
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor,
+        dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula
+        massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est
+        eleifend mi, non fermentum diam nisl sit amet erat.
+        Fusce ac felis sit amet ligula pharetra condimentum. Maecenas malesuada. Praesent congue erat at massa.
+        Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum
+        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis
+        porttitor congue, elit erat euismod orci.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor,
+        dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula
+        massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est
+        eleifend mi, non fermentum diam nisl sit amet erat.
+        Fusce ac felis sit amet ligula pharetra condimentum. Maecenas malesuada. Praesent congue erat at massa.
+        Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum
+        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis
+        porttitor congue, elit erat euismod orci.
+      </p>
 
-            <p>This is the about page</p>
+      <div style={{ clear: 'both' }} />
+
+      <p>
+        <img
+          src="/test_image_2.jpg"
+          alt="Right image"
+          className="float-right max-w-[15%] h-auto mr-4 mb-2"
+        />
+        Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris.
+        Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus
+        ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui.
+        Fusce ac felis sit amet ligula pharetra condimentum. Maecenas malesuada. Praesent congue erat at massa.
+        Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum
+        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis
+        porttitor congue, elit erat euismod orci.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor,
+        dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula
+        massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est
+        eleifend mi, non fermentum diam nisl sit amet erat.
+        Fusce ac felis sit amet ligula pharetra condimentum. Maecenas malesuada. Praesent congue erat at massa.
+        Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum
+        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis
+        porttitor congue, elit erat euismod orci.
+      </p>
+
+      <div style={{ clear: 'both' }} />
+
+      <p>
+        <img
+          src="/test_image_3.jpg"
+          alt="Left image again"
+          className="float-left max-w-[15%] h-auto mr-4 mb-2"
+        />
+        Fusce ac felis sit amet ligula pharetra condimentum. Maecenas malesuada. Praesent congue erat at massa.
+        Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum
+        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis
+        porttitor congue, elit erat euismod orci.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor,
+        dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula
+        massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est
+        eleifend mi, non fermentum diam nisl sit amet erat.
+        Fusce ac felis sit amet ligula pharetra condimentum. Maecenas malesuada. Praesent congue erat at massa.
+        Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum
+        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis
+        porttitor congue, elit erat euismod orci.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor,
+        dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula
+        massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est
+        eleifend mi, non fermentum diam nisl sit amet erat.
+        Fusce ac felis sit amet ligula pharetra condimentum. Maecenas malesuada. Praesent congue erat at massa.
+        Sed cursus turpis vitae tortor. Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum
+        ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis
+        porttitor congue, elit erat euismod orci.
+      </p>
+    </div>
         </div>
     )
 }
